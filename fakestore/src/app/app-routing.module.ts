9d7223dab20e9181540449917050df83e8f3eef7
nikka <sagcalnikkaandrea@gmail.com>
2024-09-01 18:36:31 +0800

    dwadwa
    wdawd

--- fakestore/src/app/app-routing.module.ts
+++ fakestore/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' }
 ];
-
+//nikangina
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
